6bab1845d1716e12384fe0bb8e01e1f5a51b67bd
fennzar <fennzardev@gmail.com>
2023-05-27 23:27:50 +1200

    add twitter over telegram

--- website/config.js
+++ website/config.js
@@ -3,11 +3,12 @@ var parentCoin = "Zephyr";
 var api = "http://127.0.0.1:8117";
 var poolHost = "poolhost.com";
 
-var email = "support@poolhost.com";
-var telegram = "https://t.me/YourPool";
+// var email = "support@poolhost.com";
+// var telegram = "https://t.me/YourPool";
 var discord = "https://discordapp.com/invite/YourPool";
+var twitter = "https://twitter.com/zephyr_org";
 
-var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+// var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
 var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
